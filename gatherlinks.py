e5d6b88c55ed491ec84cf1cf1f1b632c1c1a5e3f
Jack-Klingenberg <jackklingenberg1@gmail.com>
2023-11-22 14:00:29 -0800

    Adds link finder script

---
+++ gatherlinks.py
@@ -0,0 +1,28 @@
+import bs4
+import requests
+import sys 
+from tqdm import tqdm
+START = int(sys.argv[1])
+END  = int(sys.argv[2])
+TARGET = sys.argv[3]
+def check_link_exists(url):
+    try:
+        response = requests.head(url)
+        return response.status_code // 100 == 2
+    except requests.RequestException:
+        return False
+
+links = []
+print("Links processing...")
+progressbar = tqdm(total=(END-START+1)*12*31)
+for year in range(START,END+1):
+    for month in range(1,13):
+        for day in range(1,31):
+            link = "https://www.federalreserve.gov/monetarypolicy/fomcminutes%i%02i%02i.htm" %(year,month,day)
+            if check_link_exists(link):
+                links.append(link)
+            progressbar.update(1)
+            progressbar.set_postfix(Message=f"Checking date {month}/{day}/{year}")
+
+with open(TARGET, "w") as file:
+    [file.write(link+"\n") for link in links]
