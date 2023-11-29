1f2a6af4a5aad6edb127fbd978897d2455b39495
Benjamin Elenbaas <elenbaab@bc.edu>
2023-11-29 10:51:33 -0500

    read in data function

--- analyzedata.py
+++ analyzedata.py
@@ -1,12 +1,13 @@
 import sys
 import csv
 
-lines = []
-
-with open('data/ds1.csv', 'r') as csv_file, open('text.txt', 'w') as out_file:
-    for line in csv_file:
-        out_file.write(line.strip())
-        lines.append(line.strip())
+def read_in_data():
+    lines = []
+    with open('data/ds1.csv', 'r') as csv_file, open('text.txt', 'w') as out_file:
+        for line in csv_file:
+            out_file.write(line.strip())
+            lines.append(line.strip())
+    return lines
 
 #'lines' is a list of string of all the text per html file
 #can now tokenize
